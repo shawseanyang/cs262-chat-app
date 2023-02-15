a2980ae11c07c452c11b3126b0874785044672ae
Gene Lee <genlee@college.harvard.edu>
2023-02-14 23:43:24 -0500

    Completed connection to server and createAccount and deleteAccount

--- src/main/java/com/chatapp/client/Client.java
+++ src/main/java/com/chatapp/client/Client.java
@@ -1,5 +1,9 @@
 package com.chatapp.client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.Scanner;
 
 import com.chatapp.client.commands.Command;
@@ -10,12 +14,19 @@ import com.chatapp.client.commands.ListAccountsCommand;
 import com.chatapp.client.commands.LogInCommand;
 import com.chatapp.client.commands.LogOutCommand;
 import com.chatapp.client.commands.SendMessageCommand;
+import com.chatapp.client.exceptions.InvalidUsernameException;
+import com.chatapp.client.exceptions.NotLoggedInException;
+import com.chatapp.client.exceptions.UserAlreadyExistsException;
+import com.chatapp.client.exceptions.UserDoesNotExistException;
 
 // Entry point of the client application. Listens for user commands from the console and executes them. This class is responsible for UI logic. It is responsible for creating a channel to the server, and then passing the channel to the handlers.
 
 public class Client {
   static Scanner in = new Scanner(System.in);
-  // TODO: socket
+  static Socket socket;
+  // input and output streams to the server
+  static DataInputStream server_in;
+  static DataOutputStream server_out;
 
   public static void main(String[] args) {
     // listen for new user commands from the console
@@ -32,35 +43,86 @@ public class Client {
       // when its a connect command, create a new channel to the server
       if (command instanceof ConnectCommand) {
         ConnectCommand cast = (ConnectCommand) command;
-        // use cast.getHost() and cast.getPort() to connect to the server via sockets
+        // create a new socket to the server
+        try {
+          socket = new Socket(cast.getHost(), cast.getPort());
+        } catch (IOException e) {
+          System.out.println("-> Error: Could not connect to the specified server.");
+          e.printStackTrace();
+        }
         continue;
       }
       
-      // TODO: if there is no socket, then the user must connect first
-      // if (socket == null) {
-      //   System.out.println("-> Error: You must connect to a server first.");
-      //   continue;
-      // }
+      // if there is no socket, then the user must connect first
+      if (socket == null) {
+        System.out.println("-> Error: You must connect to a server first.");
+        continue;
+      }
+      
+      // create input and output streams to the server
+      try {
+        server_in = new DataInputStream(socket.getInputStream());
+        server_out = new DataOutputStream(socket.getOutputStream());
+      } catch (IOException e) {
+        System.out.println("-> Error: Could not create input and output streams to the server.");
+        e.printStackTrace();
+      }
 
       // TODO: execute the command
       if (command instanceof CreateAccountCommand) {
         CreateAccountCommand cast = (CreateAccountCommand) command;
-        
+        try {
+          ClientHandler.createAccount(cast);
+        } catch (UserAlreadyExistsException e) {
+          System.out.println("-> Error: User already exists.");
+          e.printStackTrace();
+        } catch (InvalidUsernameException e) {
+          System.out.println("-> Error: Invalid username.");
+          e.printStackTrace();
+        }
       }
       else if (command instanceof DeleteAccountCommand) {
         DeleteAccountCommand cast = (DeleteAccountCommand) command;
+        try {
+          ClientHandler.deleteAccount(cast);
+        } catch (InvalidUsernameException e) {
+          System.out.println("-> Error: Invalid username.");
+          e.printStackTrace();
+        }
       }
       else if (command instanceof ListAccountsCommand) {
         ListAccountsCommand cast = (ListAccountsCommand) command;
+        ClientHandler.listAccounts(cast);
       }
       else if (command instanceof LogInCommand) {
         LogInCommand cast = (LogInCommand) command;
+        try {
+          ClientHandler.logIn(cast);
+        } catch (UserDoesNotExistException e) {
+          System.out.println("-> Error: User does not exist.");
+          e.printStackTrace();
+        }
       }
       else if (command instanceof LogOutCommand) {
         LogOutCommand cast = (LogOutCommand) command;
+        try {
+          ClientHandler.logOut(cast);
+        } catch (NotLoggedInException e) {
+          System.out.println("-> Error: You are not logged in.");
+          e.printStackTrace();
+        }
       }
       else if (command instanceof SendMessageCommand) {
         SendMessageCommand cast = (SendMessageCommand) command;
+        try {
+          ClientHandler.sendMessage(cast);
+        } catch (UserDoesNotExistException e) {
+          System.out.println("-> Error: User does not exist.");
+          e.printStackTrace();
+        } catch (NotLoggedInException e) {
+          System.out.println("-> Error: You are not logged in.");
+          e.printStackTrace();
+        }
       }
     }
   }
--- src/main/java/com/chatapp/client/ClientHandler.java
+++ src/main/java/com/chatapp/client/ClientHandler.java
@@ -1,5 +1,8 @@
 package com.chatapp.client;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import com.chatapp.client.commands.CreateAccountCommand;
 import com.chatapp.client.commands.DeleteAccountCommand;
 import com.chatapp.client.commands.ListAccountsCommand;
@@ -10,6 +13,11 @@ import com.chatapp.client.exceptions.InvalidUsernameException;
 import com.chatapp.client.exceptions.NotLoggedInException;
 import com.chatapp.client.exceptions.UserAlreadyExistsException;
 import com.chatapp.client.exceptions.UserDoesNotExistException;
+import com.chatapp.protocol.Constants;
+import com.chatapp.protocol.Marshaller;
+import com.chatapp.protocol.Message;
+import com.chatapp.protocol.Operation;
+import com.chatapp.utility.ByteConverter;
 
 // Helps the Client handle commands from the user because the Client is responsible for the UI logic. Each of the functions handles its command bu sending requests to the server, waiting for a response, then passing the response back to the caller.
 
@@ -18,11 +26,55 @@ import com.chatapp.client.exceptions.UserDoesNotExistException;
 
 public class ClientHandler {
   public static void createAccount(CreateAccountCommand command) throws UserAlreadyExistsException, InvalidUsernameException {
+    // Convert the username to an argument
+    ArrayList<byte[]> args = new ArrayList<byte[]>();
+    args.add(ByteConverter.stringToByteArray(command.getUsername()));
 
+    // Create a message to send to the server
+    sendMessage(Operation.CREATE_ACCOUNT, com.chatapp.protocol.Exception.NONE, args);
+
+    // Wait for a response from the server
+    byte[] responseBytes = null;
+    try {
+      responseBytes = Client.server_in.readAllBytes();
+    } catch (IOException e) {
+      System.err.println("ERROR: Could not read the response from the server.");
+      e.printStackTrace();
+    }
+
+    // Unmarshall the response
+    Message response = Marshaller.unmarshall(responseBytes);
+
+    // Check the response for errors
+    if (response.getException() == com.chatapp.protocol.Exception.USER_ALREADY_EXISTS)
+      throw new UserAlreadyExistsException("The username " + command.getUsername() + " is already taken.");
+    else if (response.getException() == com.chatapp.protocol.Exception.INVALID_USERNAME)
+      throw new InvalidUsernameException("The username " + command.getUsername() + " is invalid.");
   }
 
   public static void deleteAccount(DeleteAccountCommand command) throws InvalidUsernameException {
+    // Convert the username to an argument
+    ArrayList<byte[]> args = new ArrayList<byte[]>();
+    args.add(ByteConverter.stringToByteArray(command.getUsername()));
 
+    // Create a message to send to the server
+    sendMessage(Operation.CREATE_ACCOUNT, com.chatapp.protocol.Exception.NONE, args);
+
+    // Wait for a response from the server
+    byte[] responseBytes = null;
+    try {
+      responseBytes = Client.server_in.readAllBytes();
+    } catch (IOException e) {
+      System.err.println("ERROR: Could not read the response from the server.");
+      e.printStackTrace();
+    }
+
+    // Unmarshall the response
+    Message response = Marshaller.unmarshall(responseBytes);
+
+    // Check the response for errors
+    if (response.getException() == com.chatapp.protocol.Exception.INVALID_USERNAME)
+      throw new InvalidUsernameException("The username " + command.getUsername() + " is invalid.");
   }
 
   public static void listAccounts(ListAccountsCommand command) {
@@ -40,5 +92,29 @@ public class ClientHandler {
   public static void sendMessage(SendMessageCommand command) throws UserDoesNotExistException, NotLoggedInException
   {
 
+  }
+
+  // Utility functions for sending response messages to the client
+  private static void sendMessage(Operation operation, com.chatapp.protocol.Exception exception, ArrayList<byte[]> args) {
+    Message message = new Message(Constants.CURRENT_VERSION, operation, exception, args);
+    try {
+        Client.server_out.write(Marshaller.marshall(message));
+    } catch (IOException e) {
+        if (exception == com.chatapp.protocol.Exception.NONE)
+            System.err.println("ERROR: Could not send the success message.");
+        else
+            System.err.println("ERROR: Could not send the error message.");
+        e.printStackTrace();
+
+        // Log out the user upon failure to deliver a message
+        logoutHandler();
+
+        // Close the thread
+        threadIsAlive = false;
+    }
+  }
+
+  private static void sendMessage(Operation operation, com.chatapp.protocol.Exception exception) {
+      sendMessage(operation, exception, new ArrayList<byte[]>());
   }
 }
